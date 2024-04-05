b488cf82701553d6a0aa18e07aff8f21bc3e880f
Raymond M2 <raymondyuspam@gmail.com>
2024-04-05 15:24:57 +1100

    stop at item preview problem when opening browse

--- puddle/item/views.py
+++ puddle/item/views.py
@@ -1,3 +1,67 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404, redirect
 
-# Create your views here.
+from .forms import NewItemForm, EditItemForm
+from .models import Item
+
+def items (request):
+     items = Item.objects.filter(is_sold=False)
+
+     return render(request, 'item/items.html', {
+         'item' : items,
+     })
+
+def detail(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+    related_items = Item.objects.filter(category=item.category, is_sold=False).exclude(pk=pk)[0:3]
+
+    return render(request, 'item/detail.html', {
+        'item': item,
+        'related_items' : related_items
+    })
+
+@login_required
+def new(request):
+    if request.method == 'POST':
+        form = NewItemForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            item = form.save(commit=False)
+            item.created_by = request.user
+            item.save()
+
+            return redirect('item:detail', pk=item.id)
+    else:
+        form = NewItemForm()
+
+    return render(request, 'item/form.html' , {
+        'form' : form
+    })
+
+
+
+@login_required
+def edit(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+
+    if request.method == 'POST':
+        form = EditItemForm(request.POST, request.FILES, instance=item)
+
+        if form.is_valid():
+            item.save()
+
+            return redirect('item:detail', pk=item.id)
+    else:
+        form = EditItemForm(instance=item)
+
+    return render(request, 'item/form.html' , {
+        'form' : form,
+        'title': 'Edit item',
+    })
+
+@login_required
+def delete(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+    item.delete()
+
+    return redirect('dashboard:index')
--- puddle/puddle/settings.py
+++ puddle/puddle/settings.py
@@ -27,6 +27,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+LOGIN_URL = '/login'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 # Application definition
 
@@ -39,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles', 
     'core',
     'item',
+    'dashboard',
 ]
 
 MIDDLEWARE = [
